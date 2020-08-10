2cea1a7182f6723a50a8261b533e4c22791cb233
mmexEstratec <58827822+mmexEstratec@users.noreply.github.com>
2020-08-10 18:21:43 -0500

    Miguel Mex Martinez
    Se envia documentacion de :  Caja chica FM 1.0

--- includes/logout_Expire.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once 'user_session.php';
-    $userSession = new UserSession();
-    $userSession->closeSession();
-    //$tiempoExpirado = "se acabo el tiempo";
-    header("location: index.php");
-?> 
